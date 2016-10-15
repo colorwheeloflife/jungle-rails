e5ca522bc6906d79a864e3bfd8531103acef910e
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-10-15 20:01:35 +0000

    Reviews are posting to the database, and are being presented on the product page accordingly.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,25 @@ class ReviewsController < ApplicationController
     @review = Review.find params[:id]
   end
 
+  def create
+    @review = Review.new(review_params)
+    @product = Product.find_by params['product_id']
+
+    if @review.save
+      redirect_to [@product], notice: 'Review created!'
+    else
+      puts @reviews.errors.to_a
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      :product_id
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :reviews, only: [:show, :create]
+  resources :reviews, only: [:create]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
